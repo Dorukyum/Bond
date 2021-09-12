fc81e2c51152238af9300056c133a344f4e6e6bb
Dorukyum <doruk.ak@hotmail.com>
2021-09-12 12:05:14 +0300

    Create admin cog

---
+++ cogs/admin.py
@@ -0,0 +1,56 @@
+from os import listdir
+
+from discord.ext.commands import command
+from jishaku.codeblocks import codeblock_converter
+
+from utils import Cog
+
+
+class Admin(Cog, command_attrs={"hidden": True}):
+    @command(name="eval")
+    async def _eval(self, ctx, *, code: codeblock_converter):
+        cog = self.bot.get_cog("Jishaku")
+        await cog.jsk_python(ctx, argument=code)
+
+    @command()
+    async def load(self, ctx, *, name):
+        if name != "all":
+            self.bot.load_extension(f"cogs.{name}")
+            await ctx.send(name.title() + " commands are ready.")
+        else:
+            for file in listdir("./cogs"):
+                if file.endswith(".py"):
+                    self.bot.load_extension(f"cogs.{file[:-3]}")
+            await ctx.send("All cogs have been loaded.")
+
+    @command()
+    async def unload(self, ctx, *, name):
+        if name != "all":
+            self.bot.unload_extension(f"cogs.{name}")
+            await ctx.send(name.title() + " commands are disabled.")
+        else:
+            for file in listdir("./cogs"):
+                if file.endswith(".py"):
+                    self.bot.unload_extension(f"cogs.{file[:-3]}")
+            await ctx.send("All cogs have been unloaded.")
+
+    @command()
+    async def reload(self, ctx, *, name):
+        if name != "all":
+            self.bot.reload_extension(f"cogs.{name}")
+            await ctx.send(name.title() + " commands have been updated.")
+        else:
+            for file in listdir("./cogs"):
+                if file.endswith(".py"):
+                    try:
+                        self.bot.reload_extension(f"cogs.{file[:-3]}")
+                    except Exception as e:
+                        print(e)
+            await ctx.send("All cogs have been reloaded.")
+
+    async def cog_check(self, ctx):
+        return ctx.author.id == self.bot.owner.id
+
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
