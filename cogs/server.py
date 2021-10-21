8679fbcc6eac6aec519973329acc44d2b0cc18bf
Dorukyum <doruk.ak@hotmail.com>
2021-10-21 19:43:59 +0300

    Create server cog

---
+++ cogs/server.py
@@ -0,0 +1,41 @@
+from discord.ext.commands import Context, command, has_permissions
+
+from utils import Cog
+
+
+class Server(Cog):
+    def __init__(self, bot):
+        super().__init__(bot)
+        self.staff_list = bot.get_channel(884730803588829206)
+        self.last_staff_list = None
+
+    @command()
+    @has_permissions(manage_server=True)
+    async def update_staff_list(self, ctx: Context):
+        staff_roles = [
+            881411529415729173,  # Server Manager
+            881407111211384902,  # Moderator
+            882105157536591932,  # Trainee Moderator
+            881519419375910932,  # Helper
+        ]
+        embed = discord.Embed(title="**Staff List**", color=0x2F3136)
+        embed.description = ""
+        for role in staff_roles:
+            role = ctx.guild.get_role(r)
+            valid_members = [member for member in role.members if member.top_role == role]
+            embed.description += f"{role.mention} | **{len(valid_members)}** \n"
+
+            for member in valid_members:
+                embed.description += f"> `{member.id}` {member.mention}\n"
+            embed.description += "\n"
+
+        if self.last_staff_list is not None:
+            await self.last_staff_list.edit(embed=embed)
+        else:
+            await self.staff_list_channel.purge(limit=1)
+            await self.staff_list_channel.send(embed=embed)
+        await ctx.send("Done!")
+
+
+def setup(bot):
+    bot.add_cog(Server(bot))
