c7a4303830d352d46faa8810b517d8d4ff2ad68c
Dorukyum <doruk.ak@hotmail.com>
2021-10-30 18:15:15 +0300

    Make cog management use Jishaku

--- cogs/developer.py
+++ cogs/developer.py
@@ -2,51 +2,32 @@ from os import listdir
 
 from discord.ext.commands import command
 from jishaku.codeblocks import codeblock_converter
+from jishaku.modules import ExtensionConverter
 
 from utils import Cog
 
 
 class Developer(Cog, command_attrs={"hidden": True}):
+    def __init__(self, bot) -> None:
+        super().__init__(bot)
+        self.jishaku = bot.get_cog("Jishaku")
+
     @command(name="eval")
     async def _eval(self, ctx, *, code: codeblock_converter):
-        cog = self.bot.get_cog("Jishaku")
-        await cog.jsk_python(ctx, argument=code)
+        await self.jishaku.jsk_python(ctx, argument=code)
+
+    @command(aliases=["reload"])
+    async def load(self, ctx, *files: ExtensionConverter):
+        await self.jishaku.jsk_load(ctx, *files)
 
     @command()
-    async def load(self, ctx, *, name):
-        if name != "all":
-            self.bot.load_extension(f"cogs.{name}")
-            await ctx.send(name.title() + " commands are ready.")
-        else:
-            for file in listdir("./cogs"):
-                if file.endswith(".py"):
-                    self.bot.load_extension(f"cogs.{file[:-3]}")
-            await ctx.send("All cogs have been loaded.")
+    async def unload(self, ctx, *files: ExtensionConverter):
+        await self.jishaku.jsk_unload(ctx, *files)
 
     @command()
-    async def unload(self, ctx, *, name):
-        if name != "all":
-            self.bot.unload_extension(f"cogs.{name}")
-            await ctx.send(name.title() + " commands are disabled.")
-        else:
-            for file in listdir("./cogs"):
-                if file.endswith(".py"):
-                    self.bot.unload_extension(f"cogs.{file[:-3]}")
-            await ctx.send("All cogs have been unloaded.")
-
-    @command()
-    async def reload(self, ctx, *, name):
-        if name != "all":
-            self.bot.reload_extension(f"cogs.{name}")
-            await ctx.send(name.title() + " commands have been updated.")
-        else:
-            for file in listdir("./cogs"):
-                if file.endswith(".py"):
-                    try:
-                        self.bot.reload_extension(f"cogs.{file[:-3]}")
-                    except Exception as e:
-                        print(e)
-            await ctx.send("All cogs have been reloaded.")
+    async def shutdown(self, ctx):
+        await ctx.send("Shutting down.")
+        await self.bot.close()
 
     async def cog_check(self, ctx):
         return ctx.author.id in self.bot.owner_ids
