0091ac33465c365794131dda1d352ccc2bdd6c06
Dorukyum <doruk.ak@hotmail.com>
2021-12-26 22:40:23 +0300

    Implement configuration file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from os import environ, listdir
+from os import environ, path
 from sys import argv
+from json import load, dump
+from typing import Optional
 
 import discord
 from discord.ext import commands
@@ -13,16 +15,18 @@ class PycordManager(commands.Bot):
     Tag = Tag
 
     def __init__(self):
+        config = self.load_config()
+
         super().__init__(
-            command_prefix=("p." if "-t" not in argv else "d."),
+            command_prefix=config["prefix"],
             intents=discord.Intents(members=True, messages=True, guilds=True),
-            owner_ids={543397958197182464},
+            owner_ids=config["owner_ids"],
             help_command=commands.MinimalHelpCommand(),
             allowed_mentions=discord.AllowedMentions.none(),
             activity=discord.Activity(
                 type=discord.ActivityType.listening, name="p.help"
             ),
-            debug_guilds=[881207955029110855],
+            debug_guilds=config["debug_guilds"],
         )
 
         self.on_ready_fired = False
@@ -40,12 +44,31 @@ class PycordManager(commands.Bot):
 
         for cog in ["cogs.pycord"]:  # cogs with application commands
             self.load_cog(cog)
-    
+
     def load_cog(self, cog: str) -> None:
         try:
             self.load_extension(cog)
         except Exception as e:
             print(e)
+
+    def load_config(self, update: bool = True) -> dict:
+        if not path.exists("config.json"):
+            config = {}
+        else:
+            with open("config.json", "r") as f:
+                config: dict = load(f)
+
+        config.setdefault("debug_guilds", [881207955029110855])
+        config.setdefault("owner_ids", [543397958197182464])
+        config.setdefault("prefix", ("p." if "-t" not in argv else "d."))
+
+        if update:
+            self.config = config
+        return config
+
+    def dump_config(self, config: Optional[dict] = None) -> None:
+        with open("config.json", "w") as f:
+            dump(config or self.config, f)
 
     async def on_ready(self):
         if self.on_ready_fired:
