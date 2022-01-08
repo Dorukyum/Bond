dac9d690028d4912d84ec63478d9f55303894fc4
Custom <53391982+PythonIntermediateCoder@users.noreply.github.com>
2022-01-08 16:56:22 +0200

    Make /examples
    Make /examples to match with `PycordManager.pycord_examples`

--- cogs/pycord.py
+++ cogs/pycord.py
@@ -3,6 +3,21 @@ from utils import Cog
 
 import discord
 
+class ExamplesSlashCommand(disoord.SlashCommand):
+    def __init__(self, bot, *args, **kwargs):
+        examples = bot.pycord_examples
+        async def callback(ctx, name: discord.Option(str, 'Name of the example.', choices=list(examples.keys()):
+            # do /example command here
+            await ctx.semd(
+                f"Here's the {name} example.",
+                view = discord.ui.View(
+                    discord.ui.Button(
+                        label = name,
+                        url = examples[name],
+                    )
+                )
+            )
+        super().__init__(callback, *args, **kwargs)
 
 async def getattrs(ctx):
     try:
@@ -17,7 +32,6 @@ async def getattrs(ctx):
         return [f"{path}.{x}" for x in dir(thing) if not x.startswith("_")][:25]
     except AttributeError:
         return [f"{path}.{x}" for x in dir(thing) if x.startswith(attr)][:25]
-
 
 class Pycord(Cog):
     """A cog for Pycord-related commands."""
@@ -44,27 +58,6 @@ class Pycord(Cog):
             return await ctx.respond(f"Couldn't find documentation for `{path}`.")
 
         await ctx.respond(f"```\n{cleandoc(thing.__doc__)[:1993]}```")
-
-    @discord.slash_command()
-    async def example(self, ctx, name: str = ""):
-        """Get the link of an example from the Pycord repository."""
-
-        if not name.endswith(".py"):
-            name = f"{name}.py"
-        if name.startswith("slash_"):
-            name = f"app_commands/{name}"
-        file_name = name.split("/")[-1]
-        example_name = " ".join(file_name.split("_")).split(".")[0]
-        await ctx.respond(
-            f"Here's the {example_name} example.",
-            view=discord.ui.View(
-                discord.ui.Button(
-                    label=file_name,
-                    url=f"https://github.com/Pycord-Development/pycord/tree/master/examples/{name}",
-                )
-            ),
-        )
-
 
 def setup(bot):
     bot.add_cog(Pycord(bot))
