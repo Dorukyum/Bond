98b14c6cb95ab6ef8ac45f0ca0e6aca498de3d33
Custom <53391982+PythonIntermediateCoder@users.noreply.github.com>
2022-01-08 18:14:40 +0200

    Fixing changes
    We'll remove /example command because It can't be added within a cog.

--- cogs/pycord.py
+++ cogs/pycord.py
@@ -3,22 +3,6 @@ from utils import Cog
 
 import discord
 
-class ExamplesSlashCommand(disoord.SlashCommand):
-    def __init__(self, bot, *args, **kwargs):
-        examples = bot.pycord_examples
-        async def callback(ctx, name: discord.Option(str, 'Name of the example.', choices=list(examples.keys()):
-            # do /example command here
-            await ctx.send(
-                f"Here's the {name} example.",
-                view = discord.ui.View(
-                    discord.ui.Button(
-                        label = name,
-                        url = examples[name],
-                    )
-                )
-            )
-        super().__init__(callback, *args, **kwargs)
-
 async def getattrs(ctx):
     try:
         input = ctx.options["thing"]
@@ -58,10 +42,6 @@ class Pycord(Cog):
             return await ctx.respond(f"Couldn't find documentation for `{path}`.")
 
         await ctx.respond(f"```\n{cleandoc(thing.__doc__)[:1993]}```")
-
-    @discord.application_command(cls=ExamplesSlashCommand, name='example')
-    async def _example():
-        pass
 
 def setup(bot):
     bot.add_cog(Pycord(bot))
