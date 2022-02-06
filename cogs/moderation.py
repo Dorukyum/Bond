f6498d099cee822254557adb8e4a8badc86de3b3
Custom <53391982+PythonIntermediateCoder@users.noreply.github.com>
2022-02-06 09:25:33 +0200

    Add modlogs

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -2,7 +2,7 @@ import asyncio
 from collections import namedtuple
 from contextlib import suppress
 from datetime import timedelta
-from typing import Optional
+from typing import Optional, Union
 
 import discord
 from discord.ext.commands import (
@@ -27,6 +27,12 @@ class Moderation(Cog):
     KICK = ModAction("brand_red", ":hammer:", "Kicked")
     MUTE = ModAction("dark_grey", ":mute:", "Muted")
     UNMUTE = ModAction("brand_green", ":loud_sound:", "Unmuted")
+    CHANNEL_CREATE = ModAction("yellow", ":heavy_plus_sign:", "Channel Created")
+    CHANNEL_DELETE = ModAction("dark_orange", ":heavy_minus_sign:", "Channel Deleted")
+    CHANNEL_UPDATE = ModAction("orange", ":red_circle:", "Channel Updated")
+    ROLE_CREATE = ModAction("yellow", ":heavy_plus_sign:", "Role Created")
+    ROLE_DELETE = ModAction("dark_orange", ":heavy_minus_sign:", "Role Deleted")
+    ROLE_UPDATE = ModAction("orange", ":red_circle:", "Role Updated")
 
     def __init__(self, bot) -> None:
         super().__init__(bot)
@@ -44,7 +50,7 @@ class Moderation(Cog):
             and (await GuildModel.get_or_create(id=target.guild.id))[0].automod
         )
 
-    async def mod_log(
+    async def mod_log_user(
         self,
         mod: discord.Member,
         target: discord.User,
@@ -59,6 +65,70 @@ class Moderation(Cog):
             .set_author(name=f"{mod} (ID {mod.id})", icon_url=mod.display_avatar)
             .set_thumbnail(url=target.display_avatar)
         )
+
+    async def mod_log_channel(
+        self,
+        mod: discord.Member,
+        target: Union[discord.CategoryChannel, discord.VoiceChannel, discord.TextChannel],
+        action: ModAction,
+    ) -> None:
+        embed=discord.Embed(
+            description = f"**{action.emoji} {action.text}: {target.name}**",
+            color = getattr(discord.Color, action.color)(),
+        )
+        embed.set_author(name=f"{mod} (ID {mod.id})", icon_url=mod.display_avatar)
+        if action is self.CHANNEL_UPDATE:
+            # we will send the changes happened in the channel
+            changes_of_channel = []
+            after_channel = await target.guild.fetch_channel(target.id)
+            if target.name != after_channel.name:
+                changes_of_channel.append('Name changed from {0.name} to {1.name}.'.format(target, after_channel))
+            if target.position != after_channel.position:
+                changes_of_channel.append('Position changed from {target.position+1} to {after_channel.position+1}.')
+            if isinstance(target, discord.TextChannel):
+                if target.topic != after_channel.topic:
+                    changes_of_channel.append('Topic changed from `{0.topic}` to `{1.topic}`.'.format(target, after_channel))
+                if target.is_nsfw() or after_channel.is_nsfw():
+                    changes_of_channel.append('Channel has become an NSFW Channel.')
+                else:
+                    changes_of_channel.append('Channel has become an non-NSFW Channel.')
+                if target.slowmode_delay != after_channel.slowmode_delay:
+                    changes_of_channel.append('Slowmode changed from `{0.slowmode_delay}` to `{1.slowmode_delay}`'.format(target, after_channel))
+            elif isinstance(target, discord.VoiceChannel):
+                if target.user_limit != after_channel.user_limit:
+                    changes_of_channel.append('User limit changed from `{0.user_limit}` to `{1.user_limit}`'.format(target, after_channel))
+            else:
+                if target.is_nsfw() or after_channel.is_nsfw():
+                    changes_of_channel.append('Channel has become an NSFW Channel.')
+            embed.add_field(
+                name = "Changes",
+                value = '\n'.join(changes_of_channel),
+            )
+        await self.mod_log_channel.send(embed=embed)
+
+    async def mod_log_role(
+        self,
+        mod: discord.Member,
+        target: discord.Role,
+        action: ModAction,
+    ) -> None:
+        embed=discord.Embed(
+            description = f"**{action.emoji} {action.text}: {target.name}**",
+            color = getattr(discord.Color, action.color)(),
+        )
+        embed.set_author(name=f"{mod} (ID {mod.id})", icon_url=mod.display_avatar)
+        if action is self.ROLE_UPDATE:
+            after_role = await target.guild.fetch_role(target.id)
+            changes_of_role = []
+            if target.name != after_role.name:
+                changes_of_role.append('Name changed from {0.name} to {1.name}.'.format(target, after_role))
+            if target.position != after_role.position:
+                changes_of_role.append(f'Position changed from {target.position+1} to {after_role.position+1}.')
+            embed.add_field(
+                name = "Changes",
+                value = '\n'.join(changes_of_role),
+            )
+        await self.mod_log_channel.send(embed=embed)
 
     async def mute(
         self, member: discord.Member, reason: str, duration: Optional[float] = None
@@ -232,17 +302,17 @@ class Moderation(Cog):
             limit=20, action=discord.AuditLogAction.ban
         ):
             if entry.target == user:
-                await self.mod_log(entry.user, user, entry.reason, self.BAN)
+                await self.mod_log_user(entry.user, user, entry.reason, self.BAN)
                 return
 
     @Cog.listener()
-    async def on_member_ban(self, guild: discord.Guild, user):
+    async def on_member_unban(self, guild: discord.Guild, user):
         await asyncio.sleep(2)
         async for entry in guild.audit_logs(
             limit=20, action=discord.AuditLogAction.unban
         ):
             if entry.target == user:
-                await self.mod_log(entry.user, user, entry.reason, self.UNBAN)
+                await self.mod_log_user(entry.user, user, entry.reason, self.UNBAN)
                 return
 
     @Cog.listener()
@@ -252,9 +322,68 @@ class Moderation(Cog):
             limit=20, action=discord.AuditLogAction.kick
         ):
             if entry.target == member:
-                await self.mod_log(entry.user, member, entry.reason, self.KICK)
+                await self.mod_log_user(entry.user, member, entry.reason, self.KICK)
                 return
 
+    @Cog.listener()
+    async def on_guild_channel_create(self, channel):
+        await asyncio.sleep(2)
+        async for entry in member.guild.audit_logs(
+            limit=20, action=discord.AuditLogAction.channel_create
+        ):
+            if entry.target == channel:
+                await self.mod_log_channel(entry.user, channel, self.CHANNEL_CREATE)
+                return
+
+    @Cog.listener()
+    async def on_guild_channel_delete(self, channel):
+        await asyncio.sleep(2)
+        async for entry in member.guild.audit_logs(
+            limit=20, action=discord.AuditLogAction.channel_delete
+        ):
+            if entry.target == channel:
+                await self.mod_log_channel(entry.user, channel, self.CHANNEL_DELETE)
+                return
+
+    @Cog.listener()
+    async def on_guild_channel_update(self, before, after):
+        await asyncio.sleep(2)
+        async for entry in member.guild.audit_logs(
+            limit=20, action=discord.AuditLogAction.channel_update
+        ):
+            if entry.target == before:
+                await self.mod_log_channel(entry.user, channel, self.CHANNEL_UPDATE)
+                return
+
+    @Cog.listener()
+    async def on_guild_role_create(self, role):
+        await asyncio.sleep(2)
+        async for entry in member.guild.audit_logs(
+            limit=20, action=discord.AuditLogAction.role_create
+        ):
+            if entry.target == role:
+                await self.mod_log_channel(entry.user, role, self.ROLE_CREATE)
+                return
+
+    @Cog.listener()
+    async def on_guild_role_delete(self, role):
+        await asyncio.sleep(2)
+        async for entry in member.guild.audit_logs(
+            limit=20, action=discord.AuditLogAction.role_delete
+        ):
+            if entry.target == role:
+                await self.mod_log_channel(entry.user, role, self.ROLE_DELETE)
+                return
+
+    @Cog.listener()
+    async def on_guild_role_update(self, before, after):
+        await asyncio.sleep(2)
+        async for entry in member.guild.audit_logs(
+            limit=20, action=discord.AuditLogAction.role_update
+        ):
+            if entry.target == role:
+                await self.mod_log_channel(entry.user, role, self.ROLE_UPDATE)
+                return
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
