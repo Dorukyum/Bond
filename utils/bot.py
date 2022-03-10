e6969bcce0a6efbcf9368f68958f38e5d530d90c
Custom <53391982+PythonIntermediateCoder@users.noreply.github.com>
2022-03-10 10:36:27 +0200

    Better check for pycord_examples

--- utils/bot.py
+++ utils/bot.py
@@ -83,7 +83,7 @@ class PycordManager(commands.Bot):
 
     async def on_connect(self):
         def get_files(data, path = ""):
-            return {f"{path}{d['name']}": d["html_url"] for d in data if d["type"] == "file"}
+            return {{"name": f"{path}{d['name']}", "url": d["html_url"]} for d in data if d["type"] == "file"}
 
         examples = {}
         session = self.http._HTTPClient__session
