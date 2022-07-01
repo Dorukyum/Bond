379090c25301e183aa26c4dbb059ab5b51887ae2
Dorukyum <doruk.ak@hotmail.com>
2022-07-01 13:10:16 +0300

    Update automod cog with slash commands

--- cogs/automod.py
+++ cogs/automod.py
@@ -1,8 +1,9 @@
 from contextlib import suppress
 from datetime import timedelta
+from typing import Union
 
 import discord
-from discord.ext.commands import Context, command, has_permissions
+from discord import ApplicationContext
 
 from utils import Cog, GuildModel
 
@@ -10,27 +11,30 @@ from utils import Cog, GuildModel
 class Automod(Cog):
     """A cog for the automoderation system."""
 
-    async def automod_on(self, target):
+    async def automod_on(self, target: Union[discord.Message, discord.Member]) -> bool:
         """Returns whether or not the target should be automodded."""
-        if hasattr(target, "bot") and target.bot:
-            return False
-        return (
-            target.guild
-            and (await GuildModel.get_or_create(id=target.guild.id))[0].automod
+        return bool(
+            not getattr(target, "bot", False)
+            and target.guild
+            and (guild := await GuildModel.get_or_none(id=target.guild.id))
+            and guild.automod
         )
 
-    @command(name="automod")
-    @has_permissions(manage_guild=True)
-    async def _automod(self, ctx: Context, status: bool):
+    @discord.slash_command()
+    @discord.guild_only()
+    @discord.default_permissions(manage_guild=True)
+    @discord.option(
+        "status", description="Choose True to turn automoderation on or False to turn it off."
+    )
+    async def automod(self, ctx: ApplicationContext, status: bool):
         """Set the status of automoderation for this server."""
         guild, _ = await GuildModel.get_or_create(id=ctx.guild.id)
         as_text = {True: "on", False: "off"}[status]
         if guild.automod == status:
-            return await ctx.send(f"Automod is already {as_text}.")
+            return await ctx.respond(f"Automod is already {as_text}.")
 
-        guild.automod = status
-        await guild.save()
-        await ctx.send(f"Automod turned {as_text}.")
+        await guild.update_from_dict({"automod": status}).save()
+        await ctx.respond(f"Automod turned {as_text}.")
 
     @Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -58,7 +62,9 @@ class Automod(Cog):
             age = member.joined_at - member.created_at
             if age.days < 28:
                 until = timedelta(minutes=5) + member.joined_at
-                await member.timeout(until, reason=f"Automod: Young account ({age.days} days)")
+                await member.timeout(
+                    until, reason=f"Automod: Young account ({age.days} days)"
+                )
                 with suppress(discord.HTTPException):
                     await member.send(
                         f"You have been timed out for security reasons. You will be able to speak <t:{int(until.timestamp())}:R>."
